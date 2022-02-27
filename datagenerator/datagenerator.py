ce6194cb26a48126df908b8d26c0f72b9e51f21c
Thais <ahitana1@gmail.com>
2022-02-27 15:41:44 +0100

    adding arq diag, dbCreate. Modifying datagenerator

--- datagenerator/datagenerator.py
+++ datagenerator/datagenerator.py
@@ -1,39 +1,40 @@
-# NO TOCAR
-from faker import Faker
-import keyboard
-import time
 import random
 from datetime import datetime
+import time
+import mysql.connector
 
-faker = Faker('es_ES')
-USERS_TOTAL=100
-users={}
 
-def initiate_data():
-    global users
-    for i in range(0,USERS_TOTAL):
-        user={}
-        user["id"]=faker.ssn()
-        user["name"]=faker.first_name()
-        user["last_name"]=faker.last_name()
-        user["soil_humidity"]=random.uniform(0, 100)
-        user["relative_humidity"]= random.uniform(0, 100)
-        user["temperature"] = random.uniform(-2, 40)
-        user["date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-        users[user["id"]]=user
-    return users     
+connection = mysql.connector.connect(host='192.168.1.141',
+                                     database='LetsprayDB',
+                                     user='root',
+                                     password='ZurichDb')
 
-# NO TOCAR
+SENSORS_TOTAL = 100
+sql_get_last_sensor_input = """SELECT sensorId,
+    soilHumidity,
+    relativeHumidity,
+    temperature,
+    date FROM sensorsInput WHERE sensorId =%s ORDER BY sensorsInputId DESC LIMIT 1"""
 
-while True:
-    try:  
-        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
-            print('You Exited the data generator')
-            break  
-        else:
-            users_generated=initiate_data()
-            print(users_generated)
-            time.sleep(120)
-    except Exception as err:
-        print(f"Unexpected {err}, {type(err)}")
-        break
+for i in range(1, SENSORS_TOTAL+1):
+    sensor = {}
+    sensor["id"] = i
+    cursor = connection.cursor(buffered=True)
+    get_data_tuple = (i,)
+    cursor.execute(sql_get_last_sensor_input, get_data_tuple)
+    records = cursor.fetchall()
+    if records is None or len(records) < 1:
+        sensor["soilHumidity"] = random.randint(0, 100)
+        sensor["relativeHumidity"] = random.randint(0, 100)
+        sensor["temperature"] = round(random.uniform(-2, 40),1)
+        sensor["date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    else:
+        sensor["soilHumidity"] = random.randint(
+            max(records[0]["soilHumidity"]-10, 0), min(records[0]["soilHumidity"]+10, 100))
+        sensor["relativeHumidity"] = random.randint(max(
+            records[0]["relativeHumidity"]-10, 0), min(records[0]["relativeHumidity"]+10, 100))
+        sensor["temperature"] = round(random.uniform(
+            max(records[0]["temperature"]-5, -2), min(records[0]["temperature"]+5, 40)),1)
+        sensor["date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    print(sensor)
+    time.sleep(1)
