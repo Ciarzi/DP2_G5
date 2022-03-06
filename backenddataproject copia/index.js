27f4cb0404c45eb9c1b06b9026c15c87f5cf0321
Thais <ahitana1@gmail.com>
2022-03-06 15:01:40 +0100

    correcting endpoint

--- backenddataproject copia/index.js
+++ backenddataproject copia/index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -10,40 +11,55 @@ const app = express();
 app.use(bodyParser.json());
 
 const connection = mysql.createConnection({
-    host     : 'db4free.net',
-    user     : 'user_mda',
-    password : 'r00tpass',
-    database : 'db_users_mda'
-  });
+    host: '35.187.114.126',
+    user: 'root',
+    password: 'datapdb2',
+    database: 'lestsprayDb'
+});
+
+//   app.get('/documents/:format/:type', function (req, res) {
+//     var format = req.params.format,
+//         type = req.params.type;
+//  });
+
+app.get('/:userId', (req, res) => {
+    var userId = req.params.userId
+    const sql = `SELECT sensorsInput.sensorId,
+    soilHumidity,
+    relativeHumidity,
+    temperature,
+    date FROM sensorsInput 
+    inner join sensors on sensorsInput.sensorId = sensors.sensorId
+    where sensors.userId = ?
+    ORDER BY sensorsInput.sensorsInputId DESC LIMIT 1`
+
+    connection.query(sql, userId, (err, results) => {
+        if (err) throw err;
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('not result')
+        }
+    })
+});
+
+app.post('/config', function (req, res) {
+    var post_body = req.body;
+    const obj = JSON.parse(post_body);
+    const sql = `INSERT INTO userSensorConfig (sensorId, squareMeter, soilHumidity, grass, flowerPreference, timePreference)
+    VALUES ?`;
+    var values = [obj.idusuario, obj.metros_cuadrados, obj.humedad, obj.cespedPreference, obj.florPreference, obj.regadoPreference]
+    connection.query(sql, values, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+        console.log(post_body);
+    })
+});
 
 
-  app.get('/',(req,res) =>{
-    const sql = 'SELECT * FROM Grouped_id' 
-   connection.query(sql, (err,results) => {
-       if(err) throw err;
-       if(results.length > 0) {
-           res.json(results);
-       }else {
-           res.send('not result')
-       }
-   })
-  })
+connection.connect(error => {
+    if (error) throw error;
+    console.log('database serve running')
+});
 
-  app.get('/friends',(req,res) =>{
-    const sql = 'SELECT * FROM friends_distance' 
-   connection.query(sql, (err,results) => {
-       if(err) throw err;
-       if(results.length > 0) {
-           res.json(results);
-       }else {
-           res.send('not result')
-       }
-   })
-  })
-
-  connection.connect(error => {
-      if (error) throw error;
-      console.log('database serve running')
-  });
-
-  app.listen(PORT, () => console.log(`server running on port ${PORT}`)); 
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
